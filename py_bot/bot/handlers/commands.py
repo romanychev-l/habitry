393a9679e01144b36f458f02500a4e307945d13f
Leonid <leonid20.0831@gmail.com>
2025-04-16 23:34:29 +0300

    Update start button

--- py_bot/bot/handlers/commands.py
+++ py_bot/bot/handlers/commands.py
@@ -6,19 +6,47 @@ from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode
 from aiogram.fsm.context import FSMContext
 from fluentogram import TranslatorRunner
-
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import WebAppInfo, FSInputFile
 from bot.states.start import StartSG
+from bot.config_data.config import config_settings
 
 commands_router = Router()
 
 
-@commands_router.message(CommandStart())
-async def process_start_command(
-    msg: Message,
-    state: FSMContext, 
-    i18n: TranslatorRunner,
-    dialog_manager: DialogManager
-) -> None:
-    # await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
-    await msg.answer(i18n.message.start())
-    # await start_logic(msg, state, i18n)
+# @commands_router.message(CommandStart())
+# async def process_start_command(
+#     msg: Message,
+#     state: FSMContext, 
+#     i18n: TranslatorRunner,
+#     dialog_manager: DialogManager
+# ) -> None:
+#     # await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
+#     await msg.answer(i18n.message.start())
+#     # await start_logic(msg, state, i18n)
+
+@commands_router.message(Command("start"))
+async def start(msg: types.Message, i18n: TranslatorRunner):
+    # await msg.answer('https://pmpu.site/gfit?tg_id=' + str(msg.from_user.id))
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        types.InlineKeyboardButton(
+            text=i18n.message.open(),
+            # url="https://lenichev.site/ht_front_dev/",
+            url="https://t.me/" + config_settings.BOT_USERNAME + "/app",
+            # web_app=WebAppInfo(url="https://lenichev.site/ht_front_dev/"),
+        )
+    )
+    # kb = [[KeyboardButton(text=Открыть)]]
+    # keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, one_time_keyboard=True)
+
+    # filename = "./bot/src/gigi.png"
+    # await msg.answer_photo(
+    #     photo=FSInputFile(filename),
+    #     caption="Привет",
+    #     reply_markup=builder.as_markup(),
+    # )
+    await msg.answer(
+        text=i18n.message.start(),
+        reply_markup=builder.as_markup()
+    )
--- py_bot/bot/locales/en/LC_MESSAGES/txt.ftl
+++ py_bot/bot/locales/en/LC_MESSAGES/txt.ftl
@@ -1,5 +1,16 @@
 button-ex_one = Example of a button
 
-message-start = 👇 Hello! Press the Track button
+message-start = Hello! 👋
+
+    I'm your personal assistant on the path to a healthy and happy life.
+
+    Together we can:
+    • Build healthy habits
+    • Achieve your goals
+    • Improve your quality of life
+
+    Ready to start a new chapter? Click "Open" and let's begin! 💪
+
 message-error_payment = An error occurred while processing the payment. Please contact support.
 message-payment_success = Paid
+message-open = Open 🚀
